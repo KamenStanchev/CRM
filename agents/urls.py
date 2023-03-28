ed473767d7a55a727847cf2e416ddd4073e66096
KamenStanchev <k.s@abv.bg>
2023-03-28 10:21:27 +0300

    profile-details

--- agents/urls.py
+++ agents/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('profile_update/', views.profile_update, name='profile-update'),
+    path('profile_details/<str:pk>/', views.profile_details, name='profile-details'),
+
 ]
--- agents/views.py
+++ agents/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render, redirect
 
 from . import forms
+from django.forms.models import model_to_dict
+from .models import Agent, GeneralManager, Manager, Salesman
+
+
+def profile_details(request, pk):
+    # lead = Lead.objects.get(id=pk)
+    agent = Agent.objects.get(id=pk)
+
+    if agent.is_general_manager:
+        professional_role = 'General Manager'
+        profile = GeneralManager.objects.get(user=agent)
+        profile_fields = forms.GeneralManagerForm(data=model_to_dict(GeneralManager.objects.get(id=profile.id)))
+
+    elif agent.is_manager:
+        professional_role = 'Manager'
+        profile = Manager.objects.get(user=agent)
+        profile_fields = forms.ManagerForm(data=model_to_dict(Manager.objects.get(id=profile.id)))
+
+    else:
+        professional_role = 'Salesman'
+        profile = Salesman.objects.get(user=agent)
+        profile_fields = forms.SalesmanForm(data=model_to_dict(Salesman.objects.get(id=profile.id)))
+
+
+    context = {
+            'professional_role': professional_role,
+            "object": profile,
+            "object_fields": profile_fields,
+        }
+    return render(request, "profile_details.html", context)
 
 
 def profile_update(request):
